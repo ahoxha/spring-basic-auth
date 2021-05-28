45c3bd9e265126e9f745d6144b2b108f1ea79da4
Hoxha <armend.hoxha@rabobank.nl>
2021-05-28 12:37:35 +0200

    - Removes redundant annotation @AutoConfigureMockMvc from a @WebMvcTest annotated test class. - Adds @EnableWebSecurity annotation to the SecurityConfiguration class.

--- src/test/java/org/hoxha/springbasicauth/MessagesControllerMockMvcTest.java
+++ src/test/java/org/hoxha/springbasicauth/MessagesControllerMockMvcTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.hoxha.springbasicauth.model.Message;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -19,7 +18,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest
-@AutoConfigureMockMvc
 class MessagesControllerMockMvcTest {
 
     private static final String URL = "/messages";
